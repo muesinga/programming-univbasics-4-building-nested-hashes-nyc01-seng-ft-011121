9a672083dfb89ce973f72988c190bbdadb8b6dd0
Aaron Muesing <muesing.music@gmail.com>
2020-12-29 19:46:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,11 @@
 def base_hash
+  railroads = {:railroads =>
+  }
+    "railroads" => {
+      :railraods =>
+  }
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-
 end
 
 def monopoly_with_second_tier
